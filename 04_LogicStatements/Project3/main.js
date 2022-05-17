95894fb42f4db75faf9030c6ec76b36f3e6f9bd9
FilipKufalov <filip.kufalov@hotmail.com>
2022-05-17 21:06:32 +0200

    update

--- 04_LogicStatements/Project3/main.js
+++ 04_LogicStatements/Project3/main.js
@@ -2,33 +2,19 @@ let gameSign = ['Rock', 'Paper', 'Scissors'];
 let userSignInput = prompt(`Rock, Paper or Scissors?`);
 let computerRandom = Math.floor(Math.random() * 3);
 let playerChoose;
-let ComputerChoose;
+let ComputerChoose = gameSign[computerRandom];
 let message;
 
-if (computerRandom === 0) {
-    ComputerChoose = gameSign[0]
-} else if (computerRandom === 1) {
-    ComputerChoose = gameSign[1]
-} else if (computerRandom === 2) {
-    ComputerChoose = gameSign[2]
-}
-
-switch (userSignInput) {
-    case 'Rock':
+switch (userSignInput.toLowerCase()) {
     case 'rock':
-    case 0:
         userSignInput = 0;
         playerChoose = gameSign[0];
         break;
-    case 'Paper':
     case 'paper':
-    case 1:
         userSignInput = 1;
         playerChoose = gameSign[1];
         break;
-    case 'Scissors':
     case 'scissors':
-    case 2:
         userSignInput = 2;
         playerChoose = gameSign[2];
         break;
